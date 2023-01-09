dac64e1ba6d1c7075098307f41ea936fe5255a09
kamranss <kamranss@code.edu.az>
2023-01-09 19:49:42 +0400

    Feature: Post App Project initialized

---
+++ Domain/Models/Status.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Status:BaseInterface
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime SharedDate { get; set; }
+
+
+       public void GetStatusInfo()
+        {
+
+        }
+    }
+}
